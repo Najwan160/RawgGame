55b713168748a4f825b0b9bc81602a69d4609db5
Najwan160 <najwan160@gmail.com>
2022-12-05 10:03:26 +0700

    Initial Commit

---
+++ RawgGame/RawgGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawgGameApp.swift
+//  RawgGame
+//
+//  Created by Muhammad Najwan Latief on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RawgGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
